dc0ed5deb7ac19020131380f6de5e8a0da3aad47
BoyWithUke <117911858+Boy-With-Uke@users.noreply.github.com>
2024-06-14 20:08:35 +0300

    crud classe

--- server/src/routes/Class.ts
+++ server/src/routes/Class.ts
@@ -42,18 +42,33 @@ export const classesRoutes = new Hono()
     return c.json({ classes });
   })
   .get("/:id{[0-9]+}", async (c) => {
-    const userId = Number.parseInt(c.req.param("id"));
+    const classId = Number.parseInt(c.req.param("id"));
     try {
-      const user = await prisma.user.findFirst({
+      const user = await prisma.classe.findFirst({
         where: {
-          id_user: userId,
+          id_classe: classId,
         },
         select: {
-          id_user: true,
-          nom: true,
-          prenom: true,
-          email: true,
-          niveauAccess: true,
+          id_classe: true,
+          nomClasse: true,
+          classeMatiere: {
+            select: {
+              matieres: {
+                select: {
+                  id_matiere: true,
+                  nom: true,
+                  description: true,
+                  enseignant: {
+                    select: {
+                      id_user: true,
+                      nom: true,
+                      prenom: true,
+                    },
+                  },
+                },
+              },
+            },
+          },
         },
       });
       c.status(200);
@@ -92,6 +107,71 @@ export const classesRoutes = new Hono()
       let message = "Nouvelle classe créé avec succès";
       c.status(200);
       return c.json({ message, newClasse });
+    } catch (error) {
+      c.status(500);
+      return c.json({ Error: error });
+    }
+  })
+  .delete("/:id{[0-9]+}", async (c) => {
+    const classId = Number.parseInt(c.req.param("id"));
+    
+    try {
+      const existingClasse = await prisma.classe.findFirst({
+        where: {
+          id_classe: classId,
+        },
+      });
+
+      let message;
+
+      if (!existingClasse) {
+        message = `La classe avec l\'id: ${classId} n'existe pas`;
+        c.status(404);
+        return c.json({ message });
+      } else {
+        const deletedClasse = await prisma.classe.delete({
+          where: {
+            id_classe: classId,
+          },
+        });
+        message = `La classe avec l\'id: ${classId} a été supprimé avec succès`;
+        c.status(200);
+        return c.json({ message, deletedClasse });
+      }
+    } catch (error) {
+      c.status(500);
+      return c.json({ Error: error });
+    }
+  })
+  .put("/:id{[0-9]+}", zValidator("json", postClassSchema), async (c) => {
+    const classId = Number.parseInt(c.req.param("id"));
+    const body = await c.req.valid("json");
+    try {
+      const existingClasse = await prisma.classe.findFirst({
+        where: {
+          id_classe: classId,
+        },
+      });
+
+      let message;
+
+      if (!existingClasse) {
+        message = `La classe avec l\'id: ${classId} n'existe pas`;
+        c.status(404);
+        return c.json({ message });
+      } else {
+        const updatedClasse = await prisma.classe.update({
+          where: {
+            id_classe: classId,
+          },
+          data:{
+            nomClasse: body.nomClasse,
+          }
+        });
+        message = `La classe avec l\'id: ${classId} a été modifie avec succès`;
+        c.status(200);
+        return c.json({ message, updatedClasse });
+      }
     } catch (error) {
       c.status(500);
       return c.json({ Error: error });
