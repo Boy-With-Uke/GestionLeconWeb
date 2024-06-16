06626ce76227bfff2159b4bbc9de1a32e41b4d53
BoyWithUke <117911858+Boy-With-Uke@users.noreply.github.com>
2024-06-17 02:56:29 +0300

    Hero

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -2,46 +2,45 @@ import { Button } from "./ui/button";
 import { buttonVariants } from "./ui/button";
 import { HeroCards } from "./HeroCards";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { Link } from "react-router-dom";
 
 export const Hero = () => {
   return (
     <section className="container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
       <div className="text-center lg:text-start space-y-6">
-        <main className="text-5xl md:text-6xl font-bold">
+        <main className="text-5xl md:text-4xl font-bold">
           <h1 className="inline">
-            <span className="inline bg-gradient-to-r from-[#F596D3]  to-[#D247BF] text-transparent bg-clip-text">
-              Shadcn
+            <span className="inline bg-gradient-to-r from-[#2bff79]  to-primary text-transparent bg-clip-text">
+              E-Hianatra
             </span>{" "}
-            landing page
+            ,votre ecole
           </h1>{" "}
-          for{" "}
+          modern par{" "}
           <h2 className="inline">
             <span className="inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text">
-              React
+              MESUPRES
             </span>{" "}
-            developers
+            en collaboration avec l'
           </h2>
+          <h2 className="inline bg-gradient-to-r from-yellow-200  to-yellow-500 text-transparent bg-clip-text">
+            ESMIA
+          </h2>{" "}
+          <span>pour une vie d'etudiant plus simple</span>
         </main>
 
-        <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
-          Build your React landing page effortlessly with the required sections
-          to your project.
-        </p>
-
         <div className="space-y-4 md:space-y-0 md:space-x-4">
-          <Button className="w-full md:w-1/3">Get Started</Button>
-
-          <a
-            rel="noreferrer noopener"
-            href="https://github.com/leoMirandaa/shadcn-landing-page.git"
-            target="_blank"
-            className={`w-full md:w-1/3 ${buttonVariants({
-              variant: "outline",
-            })}`}
-          >
-            Github Repository
-            <GitHubLogoIcon className="ml-2 w-5 h-5" />
-          </a>
+          <Link to="/Connexion">
+            <Button className="w-full md:w-1/3">Se connecter</Button>
+          </Link>
+          <Link to={"/Inscription"}>
+            <Button
+              variant="outline"
+              size="sm"
+              className="text-primary hover:bg-primary hover:text-white w-full md:w-1/3"
+            >
+              Inscription
+            </Button>
+          </Link>
         </div>
       </div>
 
@@ -51,8 +50,7 @@ export const Hero = () => {
       </div>
 
       {/* Shadow effect */}
-      <div className="shadow">
-      </div>
+      <div className="shadow"></div>
     </section>
   );
 };
--- frontend/src/components/HeroCards.tsx
+++ frontend/src/components/HeroCards.tsx
@@ -1,6 +1,7 @@
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Badge } from "./ui/badge";
 import { Button, buttonVariants } from "@/components/ui/button";
+import kerman from '../assets/kerman.png'
 import {
   Card,
   CardContent,
@@ -12,6 +13,7 @@ import {
 import { Check, Linkedin } from "lucide-react";
 import { LightBulbIcon } from "./Icons2";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { FacebookIcon } from "lucide-react";
 
 export const HeroCards = () => {
   return (
@@ -20,45 +22,57 @@ export const HeroCards = () => {
       <Card className="absolute w-[340px] -top-[15px] drop-shadow-xl shadow-black/10 bg-white dark:shadow-primary dark:bg-slate-900">
         <CardHeader className="flex flex-row items-center gap-4 pb-2">
           <Avatar>
-            <AvatarImage alt="" src="https://github.com/shadcn.png" />
-            <AvatarFallback>SH</AvatarFallback>
+            <AvatarImage
+              alt=""
+              src="https://www.mesupres.gov.mg/assets/front/images/sites/ministres/ministre_mesupres.jpg"
+            />
+            <AvatarFallback>Ministre</AvatarFallback>
           </Avatar>
 
           <div className="flex flex-col">
-            <CardTitle className="text-lg">John Doe React</CardTitle>
-            <CardDescription>@john_doe</CardDescription>
+            <CardTitle className="text-base">
+              RAZAFIHARISON Andriamanantena
+            </CardTitle>
+            <CardDescription>
+              Ministre de l'Enseignement Supérieur et de la Recherche
+              Scientifique
+            </CardDescription>
           </div>
         </CardHeader>
 
-        <CardContent>This landing page is awesome!</CardContent>
+        <CardContent>
+          Dès ma prise de fonction en janvier 2024, je suis déterminé à relever
+          les défis suivants : faire de l’enseignement supérieur et de la
+          recherche scientifique un réel moteur de développement de Madagascar,
+          garantir la paix sociale dans le monde universitaire et instaurer un
+          enseignement de qualité. Figure également dans les principales
+          priorités l’instauration de la bonne gouvernance.
+        </CardContent>
       </Card>
 
       {/* Team */}
       <Card className="absolute right-[20px] top-4 w-80 flex flex-col justify-center items-center drop-shadow-xl shadow-black/10 bg-white dark:shadow-primary dark:bg-slate-900">
         <CardHeader className="mt-8 flex justify-center items-center pb-2">
           <img
-            src="https://i.pravatar.cc/150?img=58"
+            src={kerman}
             alt="user avatar"
             className="absolute grayscale-[0%] -top-12 rounded-full w-24 h-24 aspect-square object-cover"
           />
-          <CardTitle className="text-center">Leo Miranda</CardTitle>
+          <CardTitle className="text-center">Kerman Arivelo</CardTitle>
           <CardDescription className="font-normal text-primary">
-            Frontend Developer
+            Developpeur Junior
           </CardDescription>
         </CardHeader>
 
         <CardContent className="text-center pb-2">
-          <p>
-            I really enjoy transforming ideas into functional software that
-            exceeds expectations
-          </p>
+          <p>Transformer vos reves en choses concretes et ma passion</p>
         </CardContent>
 
         <CardFooter>
           <div>
             <a
               rel="noreferrer noopener"
-              href="https://github.com/leoMirandaa"
+              href="https://github.com/kermanArivelo"
               target="_blank"
               className={buttonVariants({
                 variant: "ghost",
@@ -70,23 +84,15 @@ export const HeroCards = () => {
             </a>
             <a
               rel="noreferrer noopener"
-              href="https://twitter.com/leo_mirand4"
+              href="https://www.facebook.com/kerman.randrianarivelo/"
               target="_blank"
               className={buttonVariants({
                 variant: "ghost",
                 size: "sm",
               })}
             >
-              <span className="sr-only">X icon</span>
-              <svg
-                role="img"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                className="fill-foreground w-5 h-5"
-              >
-                <title>X</title>
-                <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
-              </svg>
+              <span className="sr-only">Github icon</span>
+              <FacebookIcon className="w-5 h-5" />
             </a>
 
             <a
@@ -105,44 +111,7 @@ export const HeroCards = () => {
         </CardFooter>
       </Card>
 
-      {/* Pricing */}
-      <Card className="absolute top-[150px] left-[50px] w-72  drop-shadow-xl shadow-black/10 bg-white dark:shadow-primary dark:bg-slate-900">
-        <CardHeader>
-          <CardTitle className="flex item-center justify-between">
-            Free
-            <Badge variant="secondary" className="text-sm text-primary">
-              Most popular
-            </Badge>
-          </CardTitle>
-          <div>
-            <span className="text-3xl font-bold">$0</span>
-            <span className="text-muted-foreground"> /month</span>
-          </div>
-
-          <CardDescription>
-            Lorem ipsum dolor sit, amet ipsum consectetur adipisicing elit.
-          </CardDescription>
-        </CardHeader>
-
-        <CardContent>
-          <Button className="w-full">Start Free Trial</Button>
-        </CardContent>
-
-        <hr className="w-4/5 m-auto mb-4" />
-
-        <CardFooter className="flex">
-          <div className="space-y-4">
-            {["4 Team member", "4 GB Storage", "Upto 6 pages"].map(
-              (benefit: string) => (
-                <span key={benefit} className="flex">
-                  <Check className="text-green-500" />{" "}
-                  <h3 className="ml-2">{benefit}</h3>
-                </span>
-              )
-            )}
-          </div>
-        </CardFooter>
-      </Card>
+      
 
       {/* Service */}
       <Card className="absolute w-[350px] -right-[10px] bottom-[35px]  drop-shadow-xl shadow-black/10 bg-white dark:shadow-primary dark:bg-slate-900">
@@ -151,10 +120,9 @@ export const HeroCards = () => {
             <LightBulbIcon />
           </div>
           <div>
-            <CardTitle>Light & dark mode</CardTitle>
+            <CardTitle>Moins de temps perdue</CardTitle>
             <CardDescription className="text-md mt-2">
-              Lorem ipsum dolor sit amet consect adipisicing elit. Consectetur
-              natusm.
+              Tous vos documents disponible partout
             </CardDescription>
           </div>
         </CardHeader>
