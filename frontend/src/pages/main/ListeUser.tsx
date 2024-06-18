9a068b011c526baefc9ba93cfe6808c0c9d91d05
BoyWithUke <117911858+Boy-With-Uke@users.noreply.github.com>
2024-06-18 14:56:32 +0300

    "Added OrbitingLoader, useToast, and user authentication logic to ListeUser component"

--- frontend/src/pages/main/ListeUser.tsx
+++ frontend/src/pages/main/ListeUser.tsx
@@ -1,11 +1,88 @@
+import OrbitingLoader from "@/components/OrbitingLoader";
 import Sidebar from "@/components/Sidebar";
 import Navbar from "@/components/navbar";
+import { useToast } from "@/components/ui/use-toast";
+import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const ListeUser = () => {
+  type User = {
+    id_user: string;
+    nom: string;
+    prenom: string;
+    email: string;
+    niveauAccess: string;
+  };
+
+  const [actualUser, setActualUser] = useState<User | null>(null);
+  const userCookie = Cookies.get("user");
+  const [isLoading, setIsLoading] = useState(true);
+  const { toast } = useToast();
+  const navigate = useNavigate();
+  // 1. Define your form
+  const getActualUser = async () => {
+    if (!userCookie) {
+      toast({
+        variant: "destructive",
+        title: `Erreur`,
+        description: "Vous devez vous connecter pour acceder a cette ressource",
+      });
+      navigate("/");
+      return;
+    } else {
+      try {
+        const res = await fetch(`http://localhost:5173/api/user/${userCookie}`);
+        if (!res.ok) {
+          console.error("Failed to fetch user data:", res.statusText);
+          return;
+        }
+        const data = await res.json();
+        const user: User = data.user;
+        setActualUser(user);
+        console.log(actualUser);
+        if (data.user.niveauAccess !== "ADMIN") {
+          toast({
+            variant: "destructive",
+            title: `Erreur`,
+            description: ` Ressources indisponibles pour votre niveau d'acces`,
+          });
+          navigate("/");
+        }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    // Fetch user data
+    getActualUser();
+
+    // Clear the timer if the component unmounts
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <>
-      <Navbar />
-      <Sidebar />
+      {isLoading ? (
+        <div
+          className="flex justify-center items-center bg-white dark:bg-gray-950/90"
+          style={{
+            height: "100vh",
+          }}
+        >
+          <OrbitingLoader />
+        </div>
+      ) : (
+        <>
+          <Navbar />
+          <Sidebar />
+        </>
+      )}
     </>
   );
 };
