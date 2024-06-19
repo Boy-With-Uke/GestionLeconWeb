eb6687008b5cd0d9860fd7e3ce766f6f64c59ca5
BoyWithUke <117911858+Boy-With-Uke@users.noreply.github.com>
2024-06-20 00:04:58 +0300

    "Update CSS variables and modify Test2 page component"

--- frontend/src/pages/Test2.tsx
+++ frontend/src/pages/Test2.tsx
@@ -2,8 +2,8 @@ import UserTableContent from "@/components/UserTableContent";
 
 export default function Test2() {
   return (
-    <div className="flex justify-center items-center pr-9 pl-9 min-h-screen bg-white dark:bg-slate-950">
-      <UserTableContent />
-    </div>
+    <span className="inline bg-gradient-to-r from-primary-bright to-primary text-transparent bg-clip-text">
+      E-Hianatra
+    </span>
   );
 }
