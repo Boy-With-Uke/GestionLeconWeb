910501984534f1187ec4014a400df66cf84e6193
BoyWithUke <117911858+Boy-With-Uke@users.noreply.github.com>
2024-06-27 20:22:43 +0300

    logo + Hianatra

--- server/src/routes/Stat.ts
+++ server/src/routes/Stat.ts
@@ -7,7 +7,7 @@ export const statRoutes = new Hono().get("/", async (c) => {
   try {
     const userCount = await prisma.user.count();
     const subjectCount = await prisma.matiere.count();
-    const coursCount = await prisma.cours.count();
+    const coursCount = await prisma.lecon.count();
     const teacherCount = await prisma.user.count({
       where: {
         niveauAccess: "ENSEIGNANT",
@@ -15,7 +15,9 @@ export const statRoutes = new Hono().get("/", async (c) => {
     });
 
     c.status(200);
-    return c.json({ result :{userCount, subjectCount, coursCount, teacherCount} });
+    return c.json({
+      result: { userCount, subjectCount, coursCount, teacherCount },
+    });
   } catch (error) {
     c.status(500);
     return c.json({ error });
